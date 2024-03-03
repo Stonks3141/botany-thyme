e383480fc856b1891efe57918ba97a38f29f84f6
Sam Nystrom <sam@samnystrom.dev>
2024-03-03 13:46:17 -0500

    Strip unused data from plants.json

--- src/PlantCard.jsx
+++ src/PlantCard.jsx
@@ -2,28 +2,20 @@ import { useState } from 'preact/hooks';
 import { Tag, Tile } from '@carbon/react';
 
 export default function PlantCard({ data }) {
-  const scientificName = data
-            .ScientificName
-            .split(/(?=<i>)/g)
-            .flatMap(s => s.split(/(?<=<\/i>)/g))
-            .filter(s => s.startsWith('<i>') && s.endsWith('</i>'))
-            .map(s => <em>{s.slice(3, s.length - 4)}</em>)
-            [0];
-  const listFmt = new Intl.ListFormat('en', { type: 'unit' });
-  const growthHabit = listFmt.format(data.GrowthHabits);
-  const duration = listFmt.format(data.Durations);
-  const imageUrl = `https://plants.sc.egov.usda.gov/ImageLibrary/standard/${data.ProfileImageFilename}`;
   return (
     <>
       <Tile>
-        <h4 style="text-transform: capitalize">{data.CommonName}</h4>
-        <p className="caption-01">{scientificName}</p>
+        <h4 style="text-transform: capitalize">{data.commonName}</h4>
+        <p className="caption-01">{data.scientificName}</p>
         <p style="text-transform: lowercase">
-          <Tag type="red">{data.Group}</Tag>
-          {data.Durations.map(duration => <Tag type="blue">{duration}</Tag>)}
-          {data.GrowthHabits.map(habit => <Tag type="green">{habit}</Tag>)}
+          {data.growthHabits.map(habit => <Tag type="green">{habit}</Tag>)}
+          {data.durations.map(duration => <Tag type="blue">{duration}</Tag>)}
+          <Tag type="red">{data.group}</Tag>
         </p>
-        <img src={imageUrl} style="max-width: 100%; max-height: 100%;" />
+        <img
+          src={'https://plants.sc.egov.usda.gov/ImageLibrary/standard/' + data.imageFile}
+          style="max-width: 100%; max-height: 100%;"
+        />
       </Tile>
     </>
   );
